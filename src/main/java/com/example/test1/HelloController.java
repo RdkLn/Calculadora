47bb5c545c64ff94c71e77e48f34365ab9100c61
radek <radlek19@gmail.com>
2021-10-20 12:20:40 +0200

    initial

---
+++ src/main/java/com/example/test1/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.test1;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label resultado;
+    @FXML
+    private Button num1;
+
+
+    public void onClick(ActionEvent actionEvent) {
+
+    }
+}
