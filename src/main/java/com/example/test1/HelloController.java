6450362e49a0e81d70b23f23e4393d12786dc64f
radek <radlek19@gmail.com>
2021-10-26 12:44:14 +0200

    2

--- src/main/java/com/example/test1/HelloController.java
+++ src/main/java/com/example/test1/HelloController.java
@@ -1,9 +1,9 @@
 package com.example.test1;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 public class HelloController {
     @FXML
@@ -11,8 +11,30 @@ public class HelloController {
     @FXML
     private Button num1;
 
+    String num="";
+    String operador="";
+    public void onClick(MouseEvent actionEvent) {
+        String leernum=((Button)actionEvent.getSource()).getText();
+        num=num+leernum;
+        resultado.setText(num);
+    }
 
-    public void onClick(ActionEvent actionEvent) {
-
+    public void Operation(MouseEvent mouseEvent) {
+        String leernum=((Button)mouseEvent.getSource()).getText();
+        int result=Integer.parseInt(resultado.getText());
+        switch (leernum){
+            case "%":
+                Double porcentaje=(Double.parseDouble(String.valueOf(result)))/100;
+                resultado.setText(String.valueOf(porcentaje));
+                break;
+            case "-":
+                break;
+            case "/":
+                break;
+            case "+":
+                break;
+            case "x":
+                break;
+        }
     }
 }
